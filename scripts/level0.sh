d2a5456cf8a1c39f145c6a5b398af6f4d3a790b1
AnishGRao <anishr@umich.edu>
2021-01-12 23:59:30 -0800

    pc reset

--- scripts/level0.sh
+++ scripts/level0.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 sshpass -p bandit0 ssh bandit0@bandit.labs.overthewire.org -p 2220 "exit" > /dev/null 2>&1
+echo "boJ9jbbUNNfktd78OOpsqOltutMc3MY1"
--- scripts/level2.sh
+++ scripts/level2.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-sshpass -p bandit0 ssh bandit0@bandit.labs.overthewire.org -p 2220 "ls -lart; cat ./-;" > /dev/null 2>&1
-#get the following key value
 key="CV1DtqXWVFXTvM2F0k09SHz0YwRINYA9"
 sshpass -p $key ssh bandit2@bandit.labs.overthewire.org -p 2220 "ls -lart; exit" > /dev/null 2>&1
 echo $key
